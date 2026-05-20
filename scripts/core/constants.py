29c9d9ea252d2aec001b42b4ae42c51d83bf809e
Bacchin Alberto <bacchinalb@dei.unipd.it>
2026-05-20 14:36:23 +0200

    update configs for experiments

--- scripts/core/constants.py
+++ scripts/core/constants.py
@@ -2,7 +2,7 @@
 
 
 NORM_SIZE = 1000  # canonical square side length for coordinates normalization
-SCHEMA_VERSION = "vlmdojo.dataset.v1"
+SCHEMA_VERSION = "vhloop.dataset.v1"
 SUPPORTED_PROMPT_STRATEGIES = ["simple_prompt"]
 SUPPORTED_TARGET_STRATEGIES = ["bbox_json_norm1000"]
 SUPPORTED_PEFT_STRATEGIES = ["LORA"]
